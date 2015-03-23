c39aa49c91c4c0ee353ada635cbf83bf77b2387f
Andreas <ciscoheat@gmail.com>
2015-03-23 19:21:15 +0100

    Moved the navigation state to the nav object.

--- js/app.js
+++ js/app.js
@@ -1,38 +1,7 @@
 var app = {};
-app.vm = {};
-app.vm.init = function() {
-	this.title = "Mithril Polymer Demo";
-	this.nav = [
-		{
-			url: "/",
-			name: "Home"
-		},
-		{
-			url: "/one",
-			name: "Page One"
-		},
-		{
-			url: "/two",
-			name: "Page Two"
-		}
-	];
-};
-app.vm.updateSelectedNavItem = function() {
-	var route = m.route();
-	var selectedNavItem = app.vm.nav.filter(function(el) {
-		return el.url === route;
-	});
-	this.selectedNavItem = m.prop(selectedNavItem[0] || this.nav[0]);
-	return this.selectedNavItem;
-};
 
-app.controller = function() {
-	app.vm.init();
-};
-
-app.view = function(ctrl) {
-	var vm = app.vm;
-	var selected = vm.updateSelectedNavItem()();
+app.view = function() {
+	var selected = nav.selectedItem();
 	return [
 		nav.view(selected),
 		toolbar.view(selected),
@@ -41,14 +10,35 @@ app.view = function(ctrl) {
 };
 
 var nav = {};
+nav.title = "Mithril Polymer Demo";
+nav.items = [
+	{
+		url: "/",
+		name: "Home"
+	},
+	{
+		url: "/one",
+		name: "Page One"
+	},
+	{
+		url: "/two",
+		name: "Page Two"
+	}
+];
+nav.selectedItem = function() {
+	var filtered = this.items.filter(function(el) {
+		return el.url === m.route();
+	});
+	return filtered.length > 0 ? filtered[0] : this.items[0];
+};
 nav.view = function(selected) {
 	return m("nav", [
 		m("core-toolbar", 
-			m("span", app.vm.title)
+			m("span", this.title)
 		),
-		app.vm.nav.map(function(link) {
-            return m("paper-item[noink]", {class: link === selected ? "selected" : ""},
-            	m("a", {href: link.url, config: m.route}, link.name)
+		this.items.map(function(item) {
+            return m("paper-item[noink]", {class: item.url === selected.url ? "selected" : ""},
+            	m("a", {href: item.url, config: m.route}, item.name)
             )
         })
     ]);
